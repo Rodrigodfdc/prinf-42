e965196f010bc067ff33092a552af4cfede3cf83
rodrigo <rodrigodfdc@gmail.com>
2026-05-10 20:14:37 +0200

    main prinnncipal print f

--- ft_printf.c
+++ ft_printf.c
@@ -1,1 +1,47 @@
 #include "ft_printf.h"
+
+static size_t	format(const char type, va_list args)
+{
+	size_t	count;
+
+	count = 0;
+	if (type == 'c')
+		count += ft_putchar(va_arg(args, int));
+	else if (type == 's')
+		count += ft_putstr(va_arg(args, char *));
+	else if (type == 'd' || type == 'i')
+		count += ft_putnbr(va_arg(args, int));
+	else if (type == 'u')
+		count += ft_putunsigned(va_arg(args, unsigned int));
+	else if (type == 'p')
+		count += ft_putptr(va_arg(args, void *));
+	else if (type == 'x')
+		count += ft_puthex(va_arg(args, unsigned int));
+	else if (type == 'X')
+		count += ft_puthex_upper(va_arg(args, unsigned int));
+	else if (type == '%')
+		count += ft_putchar('%');
+	return (count);
+}
+
+size_t	ft_printf(const char *str, ...)
+{
+	va_list args;
+	size_t count;
+
+	count = 0;
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			count += format(*str, args);
+		}
+		else
+			count += ft_putchat(*str);
+		str++;
+	}
+	va_end(args);
+	return (count);
+}
